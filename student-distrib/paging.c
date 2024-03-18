2edff2b4830ef2bbc589d83836f17175ac02c2d0
Sairaj Shetye <sshetye2@illinois.edu>
2024-03-18 15:01:01 -0500

    edited paging structs

--- student-distrib/paging.c
+++ student-distrib/paging.c
@@ -1,11 +1,12 @@
 #include "paging.h"
 
-page_table_entry_4kb_t page_table[1024] __attribute__((aligned(4096)));
-page_table_entry_4mb_t page_table_kernel[1024] __attribute__((aligned(4096)));
+page_table_entry_t page_table[1024] __attribute__((aligned(4096)));
+// page_table_entry_4mb_t page_table_kernel[1024] __attribute__((aligned(4096)));
 page_dir_entry_t page_directory[1024] __attribute__((aligned(4096)));
 
 void set_page_table(){
     unsigned int i;
+    // set the 0-4mB page table
     for(i = 0; i < 1024; i++) {
         // b8000 is the page for video memory
         // this is 753664 in decimal
@@ -22,52 +23,56 @@ void set_page_table(){
         page_table[i].unused_1 = 0;
         page_table[i].accessed = 0;
         page_table[i].dirty = 0;
-        page_table[i].pagesize = 0; // 4 kB size page
+        // page_table[i].pagesize = 0; // 4 kB size page
         page_table[i].unused_2 = 0;
         page_table[i].avail = 0;
         page_table[i].pf_addr = (i * 4096) >> 12; // each page is 4 kB, no need to worry about offset since 4kB aligned
     }
 
-    for(i = 0; i < 1024; i++) {
-        page_table_kernel[i].present = 1; 
-        page_table_kernel[i].readwrite = 1;       // read/write
-        page_table_kernel[i].usersupervisor = 0;  // supervisor mode
-        page_table_kernel[i].unused_1 = 0;
-        page_table_kernel[i].accessed = 0;
-        page_table_kernel[i].dirty = 0;
-        page_table_kernel[i].pagesize = 1; // 4 mB size page
-        page_table_kernel[i].unused_2 = 0;
-        page_table_kernel[i].avail = 0;
-        page_table_kernel[i].pat_unused = 0;
-        page_table_kernel[i].rsvd_unused = 0;
-        uint32_t addr_shifted = (4194304 + (i * 4096)) >> 20; // recover top 18 bits
-        // bottom 10 bits are 31-22, top 8 bits are 39-32
-        page_table_kernel[i].addr_39_32 = addr_shifted >> 10; // recover top 8 bits 
-        page_table_kernel[i].addr_31_22 = addr_shifted & 0x000003FF; // recover bottom 10 bits
-    }
+    // for(i = 0; i < 1024; i++) {
+    //     page_table_kernel[i].present = 1; 
+    //     page_table_kernel[i].readwrite = 1;       // read/write
+    //     page_table_kernel[i].usersupervisor = 0;  // supervisor mode
+    //     page_table_kernel[i].unused_1 = 0;
+    //     page_table_kernel[i].accessed = 0;
+    //     page_table_kernel[i].dirty = 0;
+    //     page_table_kernel[i].pagesize = 1; // 4 mB size page
+    //     page_table_kernel[i].unused_2 = 0;
+    //     page_table_kernel[i].avail = 0;
+    //     page_table_kernel[i].pat_unused = 0;
+    //     page_table_kernel[i].rsvd_unused = 0;
+    //     uint32_t addr_shifted = (4194304 + (i * 4096)) >> 20; // recover top 18 bits
+    //     // bottom 10 bits are 31-22, top 8 bits are 39-32
+    //     page_table_kernel[i].addr_39_32 = addr_shifted >> 10; // recover top 8 bits 
+    //     page_table_kernel[i].addr_31_22 = addr_shifted & 0x000003FF; // recover bottom 10 bits
+    // }
+
+
     // first 4 mB (0 - 4 mB)
     page_directory[0].present = 1;         // present
     page_directory[0].readwrite = 1;       // read/write
     page_directory[0].usersupervisor = 0;  // supervisor mode
-    page_directory[0].unused_1 = 0;
+    page_directory[0].unused_pwt = 0;
+    page_directory[0].unused_pcd = 0;
     page_directory[0].accessed = 0;
-    page_directory[0].dirty = 0;
-    page_directory[0].pagesize = 0;
-    page_directory[0].unused_2 = 0;
+    page_directory[0].unused_dirty = 0;
+    page_directory[0].pagesize = 0; // 4kB
+    page_directory[0].unused_global = 0;
     page_directory[0].avail = 0;
-    page_directory[0].pf_addr = ((unsigned int)page_table) >> 12;  // 12 shift fixed bootloop
+    page_directory[0].addr_31_12_or_addr_31_22 = ((unsigned int)page_table) >> 12;  // 12 shift fixed bootloop
 
     // second 4 mB (4 - 8 mB) (kernel)
     page_directory[1].present = 1;         // present
     page_directory[1].readwrite = 1;       // read only
     page_directory[1].usersupervisor = 0;  // supervisor mode
-    page_directory[1].unused_1 = 0;
+    page_directory[1].unused_pwt = 0;
+    page_directory[1].unused_pcd = 0;
     page_directory[1].accessed = 0;
-    page_directory[1].dirty = 0;
+    page_directory[1].unused_dirty = 0;
     page_directory[1].pagesize = 1;  // 4 mB size page
-    page_directory[1].unused_2 = 0;
+    page_directory[1].unused_global = 0;
     page_directory[1].avail = 0;
-    page_directory[1].pf_addr = ((unsigned int)page_table_kernel) >> 12; // 12 shift fixed bootloop
+    page_directory[1].addr_31_12_or_addr_31_22 = ((4194304 >> 12) & 0xFFC00); // 12 shift fixed bootloop
 }
 
 void blank_page_dir(){
@@ -75,16 +80,27 @@ void blank_page_dir(){
     for(i = 0; i < 1024; i++) {
         // 0x00000002
         // 0000 0000 0000 0000 0000 0000 0000 0010
-        page_directory[i].present = 0;         // not present
-        page_directory[i].readwrite = 1;       // read/write
-        page_directory[i].usersupervisor = 0;  // supervisor mode
-        page_directory[i].unused_1 = 0;
-        page_directory[i].accessed = 0;
-        page_directory[i].dirty = 0;
-        page_directory[i].pagesize = 0;
-        page_directory[i].unused_2 = 0;
-        page_directory[i].avail = 0;
-        page_directory[i].pf_addr = 0;
+        // page_directory[i].present = 0;         // not present
+        // page_directory[i].readwrite = 1;       // read/write
+        // page_directory[i].usersupervisor = 0;  // supervisor mode
+        // page_directory[i].unused_1 = 0;
+        // page_directory[i].accessed = 0;
+        // page_directory[i].dirty = 0;
+        // page_directory[i].pagesize = 0;
+        // page_directory[i].unused_2 = 0;
+        // page_directory[i].avail = 0;
+        // page_directory[i].pf_addr = 0;
+        page_directory[1].present = 0;         // present
+        page_directory[1].readwrite = 1;       // read only
+        page_directory[1].usersupervisor = 0;  // supervisor mode
+        page_directory[1].unused_pwt = 0;
+        page_directory[1].unused_pcd = 0;
+        page_directory[1].accessed = 0;
+        page_directory[1].unused_dirty = 0;
+        page_directory[1].pagesize = 1;  // 4 mB size page
+        page_directory[1].unused_global = 0;
+        page_directory[1].avail = 0;
+        page_directory[1].addr_31_12_or_addr_31_22 = 0;
     }
 }
 
--- student-distrib/paging.h
+++ student-distrib/paging.h
@@ -6,7 +6,7 @@ extern void blank_page_dir();
 extern void enable_paging();
 extern void enable_4mb_pages();
 
-typedef union page_table_entry_4kb_t {
+typedef union page_table_entry_t {
     uint32_t val;
     struct {
         uint32_t present : 1;
@@ -15,31 +15,30 @@ typedef union page_table_entry_4kb_t {
         uint32_t unused_1: 2;
         uint32_t accessed: 1;
         uint32_t dirty: 1;
-        uint32_t pagesize: 1;
-        uint32_t unused_2: 1;
+        uint32_t unused_2: 2;
         uint32_t avail: 3;
         uint32_t pf_addr: 20;
     } __attribute__ ((packed));
-} page_table_entry_4kb_t;
+} page_table_entry_t;
 
-typedef union page_table_entry_4mb_t {
-    uint32_t val;
-    struct {
-        uint32_t present : 1;
-        uint32_t readwrite: 1;
-        uint32_t usersupervisor: 1;
-        uint32_t unused_1: 2;
-        uint32_t accessed: 1;
-        uint32_t dirty: 1;
-        uint32_t pagesize: 1;
-        uint32_t unused_2: 1;
-        uint32_t avail: 3;
-        uint32_t pat_unused: 1;
-        uint32_t addr_39_32: 8;
-        uint32_t rsvd_unused: 1;
-        uint32_t addr_31_22: 10;
-    } __attribute__ ((packed));
-} page_table_entry_4mb_t;
+// typedef union page_table_entry_4mb_t {
+//     uint32_t val;
+//     struct {
+//         uint32_t present : 1;
+//         uint32_t readwrite: 1;
+//         uint32_t usersupervisor: 1;
+//         uint32_t unused_1: 2;
+//         uint32_t accessed: 1;
+//         uint32_t dirty: 1;
+//         uint32_t pagesize: 1;
+//         uint32_t unused_2: 1;
+//         uint32_t avail: 3;
+//         uint32_t pat_unused: 1;
+//         uint32_t addr_39_32: 8;
+//         uint32_t rsvd_unused: 1;
+//         uint32_t addr_31_22: 10;
+//     } __attribute__ ((packed));
+// } page_table_entry_4mb_t;
 
 
 typedef union page_dir_entry_t {
@@ -48,19 +47,22 @@ typedef union page_dir_entry_t {
         uint32_t present : 1;
         uint32_t readwrite: 1;
         uint32_t usersupervisor: 1;
-        uint32_t unused_1: 2;
+        uint32_t unused_pwt: 1;
+        uint32_t unused_pcd: 1;
         uint32_t accessed: 1;
-        uint32_t dirty: 1;
+        uint32_t unused_dirty: 1;
         uint32_t pagesize: 1;
-        uint32_t unused_2: 1;
+        uint32_t unused_global: 1;
         uint32_t avail: 3;
-        uint32_t pf_addr: 20;
+        uint32_t addr_31_12_or_addr_31_22: 20;
     } __attribute__ ((packed));
 } page_dir_entry_t;
 
+
+
 extern void load_page_dir(page_dir_entry_t* page_dir_base);
 
-extern page_table_entry_4kb_t page_table[1024] __attribute__((aligned(4096)));
-extern page_table_entry_4mb_t page_table_kernel[1024] __attribute__((aligned(4096)));
+extern page_table_entry_t page_table[1024] __attribute__((aligned(4096)));
+//extern page_table_entry_4mb_t page_table_kernel[1024] __attribute__((aligned(4096)));
 extern page_dir_entry_t page_directory[1024] __attribute__((aligned(4096)));
 
